97aa93cc2bfb96b5b73100d8cb79d6568d8e959c
Mohammad Al Samiul <alsamiul123@gmail.com>
2025-08-18 10:54:02 +0600

    accept messages route

---
+++ src/app/api/accept-messages/route.ts
@@ -0,0 +1,104 @@
+import { dbConnect } from "@/lib/dbConnect";
+import { getServerSession, User } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]/option";
+import UserModel from "@/model/user.model";
+
+export async function POST(request: Request) {
+  // Connect to the database
+  await dbConnect();
+
+  const session = await getServerSession(authOptions);
+  const user: User = session?.user;
+  if (!session || !session.user) {
+    return Response.json(
+      { success: false, message: "Not authenticated" },
+      { status: 401 }
+    );
+  }
+
+  const userId = user._id;
+  const { acceptMessages } = await request.json();
+
+  try {
+    // Update the user's message acceptance status
+    const updatedUser = await UserModel.findByIdAndUpdate(
+      userId,
+      { isAcceptingMessages: acceptMessages },
+      { new: true }
+    );
+
+    if (!updatedUser) {
+      // User not found
+      return Response.json(
+        {
+          success: false,
+          message: "Unable to find user to update message acceptance status",
+        },
+        { status: 404 }
+      );
+    }
+
+    // Successfully updated message acceptance status
+    return Response.json(
+      {
+        success: true,
+        message: "Message acceptance status updated successfully",
+        updatedUser,
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error("Error updating message acceptance status:", error);
+    return Response.json(
+      { success: false, message: "Error updating message acceptance status" },
+      { status: 500 }
+    );
+  }
+}
+
+export async function GET(request: Request) {
+  try {
+    // Connect to the database
+    await dbConnect();
+
+    // Get the user session
+    const session = await getServerSession(authOptions);
+    const user = session?.user;
+
+    // If no session/user -> not authenticated
+    if (!session || !user || !user._id) {
+      return Response.json(
+        { success: false, message: "Not authenticated" },
+        { status: 401 }
+      );
+    }
+
+    // Retrieve the user from the database
+    const foundUser = await UserModel.findById(user._id);
+
+    if (!foundUser) {
+      return Response.json(
+        { success: false, message: "User not found" },
+        { status: 404 }
+      );
+    }
+
+    // Return user's message acceptance status
+    return Response.json(
+      {
+        success: true,
+        isAcceptingMessages: foundUser.isAcceptingMessages,
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error("Error retrieving message acceptance status:", error);
+    return Response.json(
+      {
+        success: false,
+        message: "Error retrieving message acceptance status",
+      },
+      { status: 500 }
+    );
+  }
+}
